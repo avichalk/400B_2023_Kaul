5d93bdfbafc0232e5ed4a01357eca1aec4ec75ae
user41-x1-carbon <avichalkaul@email.com>
2023-01-19 14:17:01 -0700

    Added comments, changed screenshot and output to be clearer

--- homeworks/hw2/particleproperties.py
+++ homeworks/hw2/particleproperties.py
@@ -4,14 +4,15 @@ from readfile import Read
 
 def main():
     m, d, v = particleinfo("MW_000.txt", 'disk', 100)
-    print(m, np.around(d, 3), v)
+    print(f'Mass: {m}, Distance: {d}, Velocity: {v}')
 
 def particleinfo(name, ptype, no):
     """
     Takes the filename, data type, and particle number, and returns given properties of the relavant particle.
+    Inputs: filename, type of particle (as a string), the particle no. you want (not in index format)
     """
     no -= 1 ## for array indexing purposes
-    t, pno, data = Read(name)
+    t, pno, data = Read(name) ## read data from file
     
     ## finding indices for diff. particle types.
     ## there is absolutely a better way to do this,
@@ -21,19 +22,21 @@ def particleinfo(name, ptype, no):
     bulgin = np.where(data['type'] == 3)
     
     ## particle properties depending on particle type
+    ## particle type (int), mass (float, 10**10 M_sun), x, y, z (float, kpc), vx, vy, vz (float, km/s)
     if ptype=="dark":
         typ, m, x, y, z, vx, vy, vz = data[darkin][no]
     if ptype=="disk":
         typ, m, x, y, z, vx, vy, vz = data[diskin][no]
     if ptype=='bulge':
         typ, m, x, y, z, vx, vy, vz = data[bulgin][no]
-
-    ## rounding values and assigning units
+    
+    ## converting components into single 3d magnitude
+    ## and then rounding values and assigning units
     ## the smart thing to do would be to assign them all
     ## in one block. but instead I did them all in different places
-    d = (components_to_3d(x, y, z)* u.kpc).to(u.lyr)
-    v = np.around(components_to_3d(vx, vy, vz), 3) * u.km / u.s
-    return np.around(m * 10**10, 3) * u.M_sun, d, v
+    d = np.around((components_to_3d(x, y, z)* u.kpc).to(u.lyr), 3) ## convert to one mag, assign unit kpc, convert to lyr, and round to 3 decimal places
+    v = np.around(components_to_3d(vx, vy, vz), 3) * u.km / u.s ## convert to one mag, round to 3 decimal places, assign unit km/s
+    return np.around(m * 10**10, 3) * u.M_sun, d, v ## assign unit 10**10 Msun to mass, round to 3 decimal places
 
 def components_to_3d(x, y, z):
     ## Converts all 2d separated components into one 3d magnitude vector
--- homeworks/hw2/readfile.py
+++ homeworks/hw2/readfile.py
@@ -5,11 +5,13 @@ import sys
 def Read(name, ):
     """ 
     Reads data from a tab-separated text file called "MW_000.txt containing galactic simulation data. Returns the time, total no. of particles, and all the data within.
+    Inputs : filename
+    Returns : time in timestep units, no. of particles in the file, and all remaining filedata in a numpy array.
     """
     with open(name, 'r') as file:
         time = float(file.readline().split()[1]) * u.Myr ## time looks like a float in the file. should it be an int instead?
         particle_no = file.readline().split()[1]
-    data = np.genfromtxt(name, dtype=None, names=True, skip_header=3)
+    data = np.genfromtxt(name, dtype=None, names=True, skip_header=3) ## extract data from file
     return time, particle_no, data
 
 def main():
