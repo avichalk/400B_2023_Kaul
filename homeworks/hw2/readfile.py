13b9f73a89f4ebdb8a6d8f48e8473e0f14b85c12
user41-x1-carbon <avichalkaul@email.com>
2023-01-25 12:09:52 -0700

    Updated readfile.py to return total no. of particles as an int and not a string

--- homeworks/hw2/readfile.py
+++ homeworks/hw2/readfile.py
@@ -12,10 +12,10 @@ def Read(name, ):
         time = float(file.readline().split()[1]) * u.Myr ## time looks like a float in the file. should it be an int instead?
         particle_no = file.readline().split()[1]
     data = np.genfromtxt(name, dtype=None, names=True, skip_header=3) ## extract data from file
-    return time, particle_no, data
+    return time, int(particle_no), data
 
 def main():
-    Read("MW_000.txt", )
+    print(Read("MW_000.txt", ))
 
 if __name__ == "__main__":
     main()
