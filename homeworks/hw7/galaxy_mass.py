d4492fc7dddff1176e4c5bba0152e67153f5138c
user41-x1-carbon <avichalkaul@email.com>
2023-03-19 21:21:52 -0700

    hw7

---
+++ homeworks/hw7/galaxy_mass.py
@@ -0,0 +1,73 @@
+from readfile import Read
+import numpy as np
+import astropy.units as u
+import pandas as pd
+from tabulate import tabulate
+
+def component_mass(filename, ptype,):
+    t, pno, data = Read(filename) ## read data from file
+    
+    ## finding indices for diff. particle types.
+    ## there is absolutely a better way to do this,
+    ## but i can't figure it out
+    darkin = np.where(data['type'] == 1)
+    diskin = np.where(data['type'] == 2)
+    bulgin = np.where(data['type'] == 3)
+    
+    ## particle properties depending on particle type
+    ## particle type (int), mass (float, 10**10 M_sun), x, y, z (float, kpc), vx, vy, vz (float, km/s)
+    mass = 0
+    if ptype=="dark":
+        for i in data[darkin]:
+            mass += i[1] ## sums the masses of all particles belonging to this field
+    if ptype=="disk":
+        for i in data[diskin]:
+            mass += i[1]
+    if ptype=='bulge':
+        for i in data[bulgin]:
+            mass += i[1]
+
+    return np.round(mass / 1e2, 3) ## rounds masses, as they are currently in 10^10Msun we return in units of 10^12 Msun by dividing by 1e2
+
+def main():
+    output = []
+    local_group_mass = 0
+    local_group_stellar_mass = 0
+    for i in ["MW_000.txt", "M31_000.txt", "M33_000.txt"]:
+        dark = component_mass(i, "dark")
+        disk = component_mass(i, "disk")
+        bulge = 0
+        try: 
+            bulge = component_mass(i, "bulge")
+        except:
+            pass
+        total = dark + disk + bulge
+        local_group_mass += total
+        local_group_stellar_mass += disk + bulge
+
+        output.append([i.split("_")[0], dark, disk, bulge if bulge!=0 else "N/A", np.round(total, 3), np.round((bulge+disk)/total, 3)])
+   
+    ## this separate table is one of the worst solutions to a fomatting problem I've ever come up with
+    ## i feel dirty just looking at it.
+    out2 = []
+    out2.append([f"Total Mass of the Local Group [$10^{{12}} M_\odot$]: {np.round(local_group_mass, 3)}"])
+    out2.append([f"$f_{{bar}}$ of the Local Group: {np.round(local_group_stellar_mass/local_group_mass, 3)}"])
+    
+    ## define headers (with tex formatting) and save to csv file
+    headers = ["Galaxy Name", "Halo Mass [$10^{12} M_\odot$]", "Disk Mass [$10^{12} M_\odot$]", "Bulge Mass [$10^{12} M_\odot$]", "Total Mass [$10^{12} M_\odot$]", "$f_{bar}$"]
+    pd.DataFrame(output, columns=headers).to_csv("output.txt")
+    
+    ## why spend two minutes copying over the contents of the csv file and formatting them into a tex document when you can spend half an hour automating the process?
+    with open("tex/out.tex", "w") as f:
+        f.writelines("\\documentclass{article} \n \\usepackage[a4paper, total={8in, 8in}]{geometry} \n \\begin{document} \n")
+        f.writelines(tabulate(output, headers=headers, tablefmt='latex_raw'))
+        f.writelines('\n\n')
+        f.writelines(tabulate(out2, tablefmt='latex_raw'))
+        f.writelines(f"\n\n 1. Total Mass of MW vs M31: Ratio of {np.round(output[0][4]/output[1][4], 3)}. {headers[np.argmax([output[0][i]/output[1][i] for i in range(1, 3)])+1].split('[')[0]}dominates.")
+        f.writelines(f"\n\n 2. Stellar Mass of MW vs M31: Ratio of {np.round((output[0][2]+output[0][3])/(output[1][2]+output[1][3]), 3)}. As {['MW' if (output[0][2]+output[0][3])/(output[1][2]+output[1][3])>1 else 'M31'][0]} contains more stellar mass, which is the only type of mass that directly contributes to the luminosity of a galaxy, we expect it to be more luminous.")
+        f.writelines(f"\n\n 3. Dark Matter Content of MW vs M31: Ratio of {np.round(output[0][1]/output[1][1])}. Yes - despite the stark difference in stellar matter, they have almost exactly the same dark matter content!")
+        f.writelines(f"\n\n 4. Baryonic Fraction $f_{{bar}}$ for each galaxy is {', '.join(str(x) for x in [f'{output[i][0]}:{output[i][5]}' for i in range(0, 3)])}. Our baryonic fractions are universally lower than the provided value of 16\%. Here are the True Fraction / Provided Fraction values for the various galaxies: {', '.join(str(x) for x in [f'{output[i][0]}:{output[i][5]/.16}' for i in range(0, 3)])}. This discrepancy might come down to the large amounts of interstellar gas and dust between galaxies; as galaxies are mostly dark matter, their bayonic fraction is significantly lower than the rest of the universe.")
+        f.writelines("\n \\end{document}")
+
+if __name__ == "__main__":
+    main()
--- homeworks/hw7/orbit_integration.py
+++ homeworks/hw7/orbit_integration.py
@@ -21,75 +21,78 @@ import astropy.constants as const
 from IPython.display import Latex
 
 # **** import CenterOfMass to determine the COM pos/vel of M33
-
+from CenterOfMass import CenterOfMass
 
 # **** import the GalaxyMass to determine the mass of M31 for each component
-
+from galaxy_mass import component_mass
 
 # # M33AnalyticOrbit
-
-
 
 
 class M33AnalyticOrbit:
     """ Calculate the analytical orbit of M33 around M31 """
     
-    def __init__(): # **** add inputs
-                """ **** ADD COMMENTS """
+    def __init__(self, filename): # **** add inputs
+        """ **** ADD COMMENTS """
 
         ### get the gravitational constant (the value is 4.498502151575286e-06)
         self.G = const.G.to(u.kpc**3/u.Msun/u.Gyr**2).value
         
         ### **** store the output file name
-        
+        self.filename = filename
         
         ### get the current pos/vel of M33 
         # **** create an instance of the  CenterOfMass class for M33 
+        M33_COM = CenterOfMass("M33_000.txt", 2)
 
         # **** store the position VECTOR of the M33 COM (.value to get rid of units)
-
+        M33_COM_P = M33_COM.COM_P(0.1)
         # **** store the velocity VECTOR of the M33 COM (.value to get rid of units)
+        M33_COM_V = M33_COM.COM_V(*M33_COM_P).value
         
-        
-        ### get the current pos/vel of M31 
-        # **** create an instance of the  CenterOfMass class for M31 
-
+        M31_COM = CenterOfMass("M31_000.txt", 2)
         # **** store the position VECTOR of the M31 COM (.value to get rid of units)
 
+        M31_COM_P = M31_COM.COM_P(0.1)
         # **** store the velocity VECTOR of the M31 COM (.value to get rid of units)
+        M31_COM_V = M31_COM.COM_V(*M31_COM_P).value
         
-        
+        M31_COM_P, M33_COM_P = M31_COM_P.value, M33_COM_P.value
         ### store the DIFFERENCE between the vectors posM33 - posM31
         # **** create two VECTORs self.r0 and self.v0 and have them be the
         # relative position and velocity VECTORS of M33
-        
-        
+        self.r0 = M33_COM_P - M31_COM_P 
+        self.v0 = M33_COM_V - M31_COM_V
+
         ### get the mass of each component in M31 
-        ### disk
+        ### disk (ptype 2)
         # **** self.rdisk = scale length (no units)
+        self.rdisk = 5 ## kpc
 
         # **** self.Mdisk set with ComponentMass function. Remember to *1e12 to get the right units. Use the right ptype
-        
-        ### bulge
+        self.Mdisk = component_mass("M31_000.txt", 2) * 1e12
+
+        ### bulge (ptype 3)
         # **** self.rbulge = set scale length (no units)
-
+        self.rbulge = 1 ## kpc
         # **** self.Mbulge  set with ComponentMass function. Remember to *1e12 to get the right units Use the right ptype
-        
-        # Halo
-        # **** self.rhalo = set scale length from HW5 (no units)
+        self.Mbulge = component_mass("M31_000.txt", 3)
 
+        # Halo (ptype 1)
+        # **** self.rhalo = set scale length from HW5 (no units)
+        self.rhalo = 62 ## kpc. from Assignment 4 scale length
         # **** self.Mhalo set with ComponentMass function. Remember to *1e12 to get the right units. Use the right ptype
-     
+        self.Mhalo = component_mass("M31_000.txt", 1)
     
     
-    def HernquistAccel("****"): # it is easiest if you take as an input the position VECTOR 
+    def HernquistAccel(self, M, rhalo, r): # it is easiest if you take as an input the position VECTOR 
         """ **** ADD COMMENTS """
         
         ### **** Store the magnitude of the position vector
-        rmag = "****"
+        rmag = np.sqrt(r.dot(r))
         
         ### *** Store the Acceleration
-        Hern =  "****" #follow the formula in the HW instructions
+        Hern = - self.G * M / rmag * 1 / (rhalo + rmag) ** 2 * r
         # NOTE: we want an acceleration VECTOR so you need to make sure that in the Hernquist equation you 
         # use  -G*M/(rmag *(ra + rmag)**2) * r --> where the last r is a VECTOR 
         
@@ -97,8 +100,8 @@ class M33AnalyticOrbit:
     
     
     
-    def MiyamotoNagaiAccel("****"):# it is easiest if you take as an input a position VECTOR  r 
-               """ **** ADD COMMENTS """
+    def MiyamotoNagaiAccel(self, M, r_d, r):# it is easiest if you take as an input a position VECTOR  r 
+        """ **** ADD COMMENTS """
 
         
         ### Acceleration **** follow the formula in the HW instructions
@@ -109,50 +112,57 @@ class M33AnalyticOrbit:
         #  multiply the whle thing by :   np.array([1,1,ZSTUFF]) 
         # where ZSTUFF are the terms associated with the z direction
         
+        R = np.sqrt(r[0]**2 + r[1]**2)
+        B = self.rdisk + np.sqrt(r[2]**2 + self.rdisk)
+        a = - self.G * M * r / (R**2 + B**2) ** 1.5 \
+                * np.array([1, 1, B/np.sqrt(r[2]**2 + self.rdisk**2)])
         
        
-        return 
+        return a
         # the np.array allows for a different value for the z component of the acceleration
      
     
-    def M31Accel("****"): # input should include the position vector, r
-                       """ **** ADD COMMENTS """
+    def M31Accel(self, r): # input should include the position vector, r
+        """ **** ADD COMMENTS """
 
         ### Call the previous functions for the halo, bulge and disk
         # **** these functions will take as inputs variable we defined in the initialization of the class like 
         # self.rdisk etc. 
             
             # return the SUM of the output of the acceleration functions - this will return a VECTOR 
-        return 
+        a_bulge_halo = self.HernquistAccel((self.Mhalo + self.Mbulge), self.rhalo, r)
+        a_disk = self.MiyamotoNagaiAccel(self.Mdisk, self.rdisk, r)
+
+        return np.sqrt(a_bulge_halo.dot(a_disk))
     
     
     
-    def LeapFrog("****"): # take as input r and v, which are VECTORS. Assume it is ONE vector at a time
-                               """ **** ADD COMMENTS """
+    def LeapFrog(self, dt, r, v): # take as input r and v, which are VECTORS. Assume it is ONE vector at a time
+        """ **** ADD COMMENTS """
         
         # predict the position at the next half timestep
-        rhalf = 
+        rhalf = r + v * dt/2
         
         # predict the final velocity at the next timestep using the acceleration field at the rhalf position 
-        vnew = 
+        vnew = v + self.M31Accel(rhalf) * dt
         
         # predict the final position using the average of the current velocity and the final velocity
         # this accounts for the fact that we don't know how the speed changes from the current timestep to the 
         # next, so we approximate it using the average expected speed over the time interval dt. 
-        rnew = 
+        rnew = rhalf + vnew * dt/2
         
-        return # **** return the new position and velcoity vectors
+        return rnew, vnew # **** return the new position and velcoity vectors
     
     
     
     def OrbitIntegration(self, t0, dt, tmax):
-         """ **** ADD COMMENTS """
-
+        """ **** ADD COMMENTS """
+        
         # initialize the time to the input starting time
-        t = "****"
+        t = t0
         
         # initialize an empty array of size :  rows int(tmax/dt)+2  , columns 7
-        orbit = np.zeros("****")
+        orbit = np.zeros((int(tmax/dt)+2, 7))
         
         # initialize the first row of the orbit
         orbit[0] = t0, *tuple(self.r0), *tuple(self.v0)
@@ -164,31 +174,35 @@ class M33AnalyticOrbit:
         i = 1 # since we already set the 0th values, we start the counter at 1
         
         # start the integration (advancing in time steps and computing LeapFrog at each step)
-        while ("****"):  # as long as t has not exceeded the maximal time 
-            
+        while t<tmax:  # as long as t has not exceeded the maximal time 
+            t += dt
             # **** advance the time by one timestep, dt
            
             # **** store the new time in the first column of the ith row
-            orbit["****"] =
+            orbit[i, 1] = t
             
             # ***** advance the position and velocity using the LeapFrog scheme
             # remember that LeapFrog returns a position vector and a velocity vector  
             # as an example, if a function returns three vectors you would call the function and store 
             # the variable like:     a,b,c = function(input)
             
-         
+            pos, vel = self.LeapFrog(dt, orbit[i-1, 1:4], orbit[i-1, 4:])
     
             # ****  store the new position vector into the columns with indexes 1,2,3 of the ith row of orbit
             # TIP:  if you want columns 5-7 of the Nth row of an array called A, you would write : 
             # A[n, 5:8] 
             # where the syntax is row n, start at column 5 and end BEFORE column 8
             
+            orbit[i, 1:4] = pos
+            orbit[i, 4:] = vel
             
             # ****  store the new position vector into the columns with indexes 1,2,3 of the ith row of orbit
             
             
             # **** update counter i , where i is keeping track of the number of rows (i.e. the number of time steps)
-        
+            i += 1 
+
+            print(f"{t} of {tmax}")
         
         
         # write the data to a file
@@ -198,3 +212,5 @@ class M33AnalyticOrbit:
         
         # there is no return function
 
+M33 = M33AnalyticOrbit("outfile.txt", )
+M33.OrbitIntegration(0, 0.1, 10)
