1cc63ee03b80f295fdffac41879b6714f57d25c7
user41-x1-carbon <avichalkaul@email.com>
2023-04-18 00:10:36 -0700

    Research Assignment 5 (currently taking very long to run)

---
+++ project/ResearchAssignment5/mass_distribution.py
@@ -0,0 +1,260 @@
+from CenterOfMass import CenterOfMass
+from readfile import Read
+import astropy.units as u
+import numpy as np
+from astropy.constants import G
+import matplotlib.pyplot as plt
+
+## convert G to requisite units
+G = G.to(u.kpc * u.km**2 / u.s**2 / u.M_sun)
+
+class MassProfile:
+    def __init__(self, galaxy, snapshot):
+        """ Initializing the Mass Profile class.
+
+        Inputs:
+            galaxy: string
+               Name of the galaxy we are examining.
+            snapshot: int
+                Snapshot number
+
+        Outputs:
+            None
+        
+        """
+        self.filename = f'{galaxy}_{["000"+str(snapshot)][0][-3:]}.txt'
+        self.gname = galaxy
+        self.time, self.total, self.data = Read(self.filename)
+
+        self.m = self.data['m']
+        self.x = self.data['x'] * u.kpc
+        self.y = self.data['y'] * u.kpc
+        self.z = self.data['z'] * u.kpc
+
+    def MassEnclosed(self, ptype, r):
+        """ Function to return the mass of a given particle type within a given radius.
+
+        Inputs:
+            ptype: int
+                Particle type. 1: Disk. 2: Disk. 3. Bulge.
+            r: float or array of floats
+                Distance from the center of the galaxy in kpc
+
+        Outputs:
+            mass: numpy array
+                Array of masses in units of Msun
+        """
+        if type(r) == float:
+            r = [r]
+        COM = CenterOfMass(self.filename, ptype)
+        COM_P = COM.COM_P(0.1)
+        
+        index = np.where(self.data['type'] == ptype)
+        
+        masses = []
+        r2 = np.sqrt((self.x[index] - COM_P[0])**2+(self.y[index] - COM_P[1])**2+(self.z[index] - COM_P[2])**2)
+        
+        for i in r:
+            index2 = np.where(r2.value <= i)
+            masses.append(np.sum(self.m[index][index2]))
+
+        return np.array(masses) * 1e10 * u.M_sun
+    
+    def MassEnclosedTotal(self, r,):
+        """ Returns the total mass enclosed within a given radius.
+
+        Inputs:
+            r: float or array of floats
+                Distance from the center of the galaxy in kpc.
+
+        Outputs:
+            mass: numpy array
+                Array of masses in units of Msun
+
+        """
+        x = []
+        for i in range(1, 4):
+            if self.gname == "M33": ## checking for M33 ptype: 3
+                if i == 3:
+                    break
+            if x == []:
+                x = self.MassEnclosed(i, r) ## adding up total masses
+            else:
+                x += self.MassEnclosed(i, r)
+        return np.array(x) * u.M_sun
+
+    def HernquistMass(self, r, a, Mhalo):
+        """ Function that defines Hernquist (1990) mass profile.
+    
+    Inputs:
+        r: astropy quantity
+            galactocentric distance in kpc
+        
+        a: astropy quantity
+            scale radius of the Hernquist profile in kpc
+        
+        m_halo: float
+            total halo mass in units of 1e12 M_sun
+    
+    Output:
+        mass: astropy quantity
+            total mass within the given radius in M_sun
+        """
+        return (Mhalo*r**2)/((a+r)**2)
+
+    def CircularVelocity(self, ptype, r):
+        """ Function to calculate the circular velocity of a given particle type at a given radius.
+        Inputs:
+            ptype: int
+                Particle type. 1: Halo. 2: Disk. 3. Bulge.
+            r: float or array of floats
+                Distance from the center of the galaxy in kpc
+
+        Outputs:
+            velocities: numpy array
+                Array of velocities in units of km/s
+
+        """
+        return np.round(np.sqrt(G*self.MassEnclosed(ptype, r)/r), 2)
+
+    def TotalCircularVelocity(self, r):
+        """  Function to calculate the circular velocity for all particles at a given radius.
+        Inputs:
+            r: float or array of floats
+                Distance from the center of the galaxy in kpc
+
+        Outputs:
+            velocities: numpy array
+                Array of velocities in units of km/s
+
+        """
+        return np.sqrt(G*self.MassEnclosedTotal(r)/r)
+
+    def HernquistVCirc(self, r, a, Mhalo):
+        """Function that returns velocities inside a given radius following a Hernquist (1990) mass profile.
+    
+    Inputs:
+        r: astropy quantity
+            galactocentric distance in kpc
+        
+        a: astropy quantity
+            scale radius of the Hernquist profile in kpc
+        
+        m_halo: float
+            total halo mass in units of 1e12 M_sun
+    
+    Output:
+        velocity: astropy quantity
+            velocity of particles in units of km/s
+        """
+        return np.round(np.sqrt(G*self.HernquistMass(r, a, Mhalo)/r), 2)
+
+def main():
+    fig, ax = plt.subplots(2, 3, sharey="row", sharex=True, figsize=(14, 6))
+
+    ## iterate over the different galaxies
+    for index, galaxy_name in enumerate(['M33', 'MW','M31',], 0): 
+        
+        ## Mass Calculation 
+        print(f"Calculating for {galaxy_name}...")
+
+        ## finding the rotation curves
+        M = MassProfile(galaxy_name, 0)
+        r = np.arange(0.01, 30, 0.01)
+    
+        ## iterate over all different particle types
+        for i, ptype in {1: ("Halo", "dashdot"), 2:("Disk", "--"), 3:("Bulge", ":"),}.items():
+            if M.gname == "M33":
+                if i == 3:
+                    break
+
+            ## plot mass and velocities for each particle type
+            print(f"	Mass for {M.gname}. Ptype: {ptype[0]}")
+            ax[0, index].semilogy(r, M.MassEnclosed(i, r), linestyle=ptype[1], linewidth=3, 
+                   label=ptype[0])
+
+            print(f"	Velocity for {M.gname}. Ptype: {ptype[0]}")
+            ax[1, index].semilogy(r, M.CircularVelocity(i, r), linestyle=ptype[1], linewidth=3, 
+                   label=ptype[0])
+        
+        ## plot total mass and velocity
+        print("        Total Mass and Velocities.")
+        ax[1, index].semilogy(r, M.TotalCircularVelocity(r), linestyle=(5, (10, 3)), linewidth=3, 
+                   label="Total")
+        ax[0, index].semilogy(r, M.MassEnclosedTotal(r), linestyle=(5, (10, 3)), linewidth=3, 
+                   label="Total")
+
+        # setting up Hernquist profiles
+        actual_mass = M.MassEnclosed(1, r)
+        rmse_mass = None
+        a_final_mass = None
+        final_mass = []
+        
+        actual_velocity = M.CircularVelocity(1, r)
+        rmse_velocity = None
+        a_final_velocity = None
+        final_velocity = []
+
+        full_halo_mass_index = np.where(M.data["type"] == 1)[0]
+        full_halo_mass = np.sum(M.data["m"][full_halo_mass_index] * 1e10 * u.M_sun)
+ 
+        ## fitting the Hernquist Profile
+        for a in range(1, 70):
+            print(f"        Hernquist mass + velocity for {M.gname}. Progress: {a}/69.", end='\r')
+
+            ## find masses and velocities predicted by the Hernquist profile
+            predicted_mass = M.HernquistMass(r, a, full_halo_mass)
+            predicted_velocity = M.HernquistVCirc(r, a, full_halo_mass)
+
+            ## normalized RMSE
+            rmse_new_mass = np.linalg.norm(predicted_mass - actual_mass) / np.sqrt(len(actual_mass)) / np.mean(actual_mass) 
+            rmse_new_velocity = np.linalg.norm(predicted_velocity - actual_velocity) / np.sqrt(len(actual_velocity)) / np.mean(actual_velocity) 
+            
+            ## find min rmse separately for velocity and mass
+            if rmse_mass == None or rmse_new_mass < rmse_mass:
+                rmse_mass = rmse_new_mass
+                a_final_mass = a
+                final_mass = predicted_mass
+        
+            if rmse_velocity == None or rmse_new_velocity < rmse_velocity:
+                rmse_velocity = rmse_new_velocity
+                a_final_velocity = a
+                final_velocity = predicted_velocity
+        
+        print()
+
+        ## plot Hernquist profile info for both mass and velocity
+        ## and writes the accompanying text
+        ax[0, index].semilogy(r, final_mass,  color="fuchsia", linewidth=3, 
+                   label=f"Hernquist.")
+
+        ax[0, index].text(0.05, 0.95, f"{galaxy_name}", transform=ax[0, index].transAxes, size=11)
+        ax[0, index].text(0.05, 0.90, f"Normalized RMSE: {rmse_mass.value:2e}.", transform=ax[0, index].transAxes, size=11)
+        ax[0, index].text(0.05, 0.85, f"$a_{{final}}={a_final_mass}$", transform=ax[0, index].transAxes, size=11)
+        
+        
+        ax[1, index].semilogy(r, final_velocity,  color="fuchsia", linewidth=3, 
+                   label=f"Hernquist.")
+        ax[1, index].text(0.15, 0.05, f"{galaxy_name}", transform=ax[1, index].transAxes, size=11)
+        ax[1, index].text(0.15, 0.1, f"Normalized RMSE: {rmse_velocity.value:2e}.", transform=ax[1, index].transAxes, size=11)
+        ax[1, index].text(0.15, 0.15, f"$a_{{final}}={a_final_velocity}$", transform=ax[1, index].transAxes, size=11)
+    
+
+    fig.suptitle("Mass Distributions and Velocity Curves in M31, M33, and the MW") 
+    
+    ax[1, 0].set_xlabel("R [kpc]")
+    ax[0, 0].set_ylabel(r"Mass [$M_{sun}$]")
+    ax[1, 0].set_ylabel("V [km/s]")
+    
+    plt.plot()
+    ax[0, 2].legend(loc="lower right")
+    ax[1, 2].legend(loc="lower right")
+    
+    ## special legends for M33
+    ax[0, 0].legend(loc="lower right")
+    ax[1, 0].legend(loc="upper right")
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- project/ResearchAssignment5/mass_transfer.py
+++ project/ResearchAssignment5/mass_transfer.py
@@ -2,12 +2,17 @@
 ## For this assignment, we will only concern ourselves with the position of the transferred mass, as opposed to the kinematics.
 ## We will be creating a plot showing the mass transferred between the two galaxies at different snapshots.
 
+
 import numpy as np # type: ignore
 import astropy.constants as const
+import astropy.units as u
 import matplotlib.pyplot as plt
 
 from readfile import Read
 from CenterOfMass import CenterOfMass
+from mass_distribution import MassProfile
+from astropy.constants import G
+G = G.to(u.kpc * u.km**2 / u.s**2 / u.M_sun)
 
 class GalaxyPos:
     def __init__(self, galaxy_name):
@@ -23,20 +28,109 @@ class GalaxyPos:
 
         return self.x, self.y
 
-    def
+    def potential(self, ptype):
+        ## i understand fitting to a Hernquist profile, but how to find the scale radius?
+        pass
 """
 At each timestep, calculate all particle potential energy and kinetic energy. use hernquist profile and COM to find which particles are bound to what. 
 So, at each timestep, a new class is instantiated.
 """
 
+class GenerateEnergies:
+    def __init__(self, snapshot, galaxy):
+        """
+        Generate energies for each particle and save back into a file
+        that can be read from for future work.
+
+        We calculate the binding for all particles of one galaxy first, then all particles of the second galaxy.
+        """
+        self.name = self.filename(snapshot, galaxy) 
+        self.shot = snapshot
+        self.galaxy = galaxy
+
+        self.time, self.total, self.data = Read(self.name)
+
+        self.m = np.transpose(self.data['m'])
+        self.x = np.transpose(self.data['x']) #* u.kpc
+        self.y = np.transpose(self.data['y']) #* u.kpc
+        self.z = np.transpose(self.data['z']) #* u.kpc
+        
+        self.vx = np.transpose(self.data['vx'])
+        self.vy = np.transpose(self.data['vy'])
+        self.vz = np.transpose(self.data['vz'])
+
+
+    def filename(self, snapshot, galaxy):
+        return f'{galaxy}_{["000"+str(snapshot)][0][-3:]}.txt'
+
+    def KE(self,):
+        """
+        Generates KE for each particle in the dataset.
+        KE = 1/2mv^2. Returns a sequential array.
+        Inputs:
+            self.data : `np array of floats`
+                Contains m : mass in 1e10 solar masses
+                vx, vy, vz : component velocities in km/s
+        """
+        print(f"Calculating KE.")
+        v_squared = self.vx**2 + self.vy**2 + self.vz**2 
+        return 0.5 * self.m * v_squared * (u.km / u.s)**2 ## * 1e10 * u.Msun * 
+
+    def V(self,):
+        """
+        Generates V for each particle in the dataset.
+        We find the mass enclosed using a Hernquist profile.
+        Unclear ATM how accurate this is.
+        Inputs:
+            self.data : `np array of floats`
+                Contains m : mass in 1e10 solar masses
+                vx, vy, vz : component velocities in km/s
+        Calculates separately for MW and M31, and returns in array.
+        """
+        ## permutations of measuring V?
+        ## this entire function to be called twice for both data (MW and M31)
+        out = []
+        for i in {"MW", "M31"}:
+            print(f"Calculating V for {i}.")
+            COM = CenterOfMass(self.filename(self.shot, i), 2)
+            COM_p = COM.COM_P(0.1).value
+            a = self.x - COM_p[0]
+            b = self.y - COM_p[1]
+            c = self.z - COM_p[2]
+            r = np.sqrt(np.square(a)+np.square(b)+np.square(c))
+            Profile = MassProfile(i, self.shot) ## ansc inputs, don't actually work
+            print("Calculating mass enclosed")
+            M = Profile.MassEnclosedTotal(r)
+            print("doing V")
+            V = - G * M / r
+            np.savetxt(f"{i}", V)
+            out.append(V)
+        return out
+
+    def check_if_bound(self,):
+        """
+        Check total energy. If -ve, it is gravitationally bound to that galaxy.
+        If positive, unbound. Check against both galaxies.
+        """
+        KE = self.KE()
+        MW_V, M31_V = self.V()
+        
+        print(f"Calculating Total E.")
+        E_MW = KE.value + MW_V.value
+        E_M31 = KE.value + M31_V.value
+
+        return np.sign(E_MW), np.sign(E_M31)
+
+
+
 def main():
     ## i'm going to plot each different particle type in its own plot
-    MW = GalaxyPos("MW_000.txt")
-    M31 = GalaxyPos("M31_000.txt")
-    
-    ## Plot COM so we can see how it changes over time. We can also use this to fit a Hernquist profil
-    MW_COM = CenterOfMass("MW_000.txt", 2)
-    #MW_COM_p = MW_COM.COM_P()
+    #MW = GalaxyPos("MW_000.txt")
+    #M31 = GalaxyPos("M31_000.txt")
+    #
+    ### Plot COM so we can see how it changes over time. We can also use this to fit a Hernquist profil
+    #MW_COM = CenterOfMass("MW_000.txt", 2)
+    ##MW_COM_p = MW_COM.COM_P()
 
     ### loop over all particles and check a) unbound to host galaxy b) bound to new galaxy. if neither are true, assume still bound to host galaxy
     #
@@ -47,6 +141,12 @@ def main():
 
     #ax.legend()
     #plt.show()
+
+    for i in {"MW", "M31"}:
+        E = GenerateEnergies(0, i)
+        res = E.check_if_bound()
+        print(res)
+
 
 if __name__ == "__main__":
     main()
